f84259e97fcbe03e9d1256eb6af469ec4246ff76
32-52 <alexeysvetlov92@gmail.com>
2019-11-02 17:53:54 +0300

    Added location class and get_nearest_bus method

--- schedule_module.py
+++ schedule_module.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import datetime
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 from typing import List
@@ -15,22 +16,30 @@ logging.basicConfig(
     ])
 
 
+class Location:
+    def __init__(self, name: str, url: str):
+        self.name = name
+        self.url = url
+
+
 class Locations(Enum):
-    MARINA_RISHHA = 'https://transport.lanit.ru/m/table'
-    PLOSHHAD_ILICHA = 'https://transport.lanit.ru/p/table'
-    RIZHSKAJA = 'https://transport.lanit.ru/r/table'
+    MARINA_RISHHA = Location(
+        'Марьина роща', 'https://transport.lanit.ru/m/table')
+    PLOSHHAD_ILICHA = Location(
+        'Площадь Ильича', 'https://transport.lanit.ru/p/table')
+    RIZHSKAJA = Location('Рижская', 'https://transport.lanit.ru/r/table')
 
 
 class Destinations(Enum):
-    TO_METRO = 1
-    TO_OFFICE = 2
+    TO_METRO = 'к метро'
+    TO_OFFICE = 'к офису'
 
 
 class LanitBusInfo:
     @staticmethod
     def get_schedule_info(location: Locations) -> dict:
         logging.info('Get schedule info started...')
-        response = requests.get(location.value)
+        response = requests.get(location.value.url)
 
         soup = BeautifulSoup(response.text, 'html.parser')
         tables: List[Tag] = soup.findAll("div", {"class": "col-xs-6"})
@@ -46,11 +55,18 @@ class LanitBusInfo:
         logging.info('Get schedule info completed')
         return schedule_data
 
+    @staticmethod
+    def get_nearest_bus(location: Locations, destinations: Destinations) -> str:
+        current_time = datetime.datetime.now()
+        formated_current_time = f'{current_time.hour}:{current_time.minute}'
+        if datetime.datetime.today().weekday() < 5:
+            return ''
+        else:
+            return f'Сейчас {formated_current_time}. Сегодня маршруток {destinations.value} {location.value.name} уже не будет.'
+
 
 if __name__ == "__main__":
-    marina = LanitBusInfo.get_schedule_info(Locations.MARINA_RISHHA)
-    logging.info(f'marina {type(marina)} = {marina}')
-    ilicha = LanitBusInfo.get_schedule_info(Locations.PLOSHHAD_ILICHA)
-    logging.info(f'ilicha {type(ilicha)} = {ilicha}')
-    rizhskaja = LanitBusInfo.get_schedule_info(Locations.RIZHSKAJA)
-    logging.info(f'rizhskaja {type(rizhskaja)} = {rizhskaja}')
+    var_name = datetime.datetime.today().weekday()
+    qweasd = LanitBusInfo.get_nearest_bus(
+        Locations.PLOSHHAD_ILICHA, Destinations.TO_METRO)
+    logging.info(f'qweasd {type(qweasd)} = {qweasd}')
