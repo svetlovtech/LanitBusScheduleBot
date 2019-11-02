081e52b35b3e229f4759934457bc5cddc6ab5c2e
32-52 <alexeysvetlov92@gmail.com>
2019-11-02 16:42:47 +0300

    Created lanitbusinfo class

--- schedule_module.py
+++ schedule_module.py
@@ -1,6 +1,9 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+from typing import List
+from enum import Enum
 import logging
 
 logging.basicConfig(
@@ -11,15 +14,38 @@ logging.basicConfig(
         logging.StreamHandler()
     ])
 
+
+class Destinations(Enum):
+    MARINA_RISHHA = 'https://transport.lanit.ru/m/table'
+    PLOSHHAD_ILICHA = 'https://transport.lanit.ru/p/table'
+    RIZHSKAJA = 'https://transport.lanit.ru/r/table'
+
+
+class LanitBusInfo:
+    @staticmethod
+    def get_schedule_info(destinations: Destinations):
+        logging.info('Get schedule info started...')
+        response = requests.get(destinations.value)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tables: List[Tag] = soup.findAll("div", {"class": "col-xs-6"})
+        schedule_data = {}
+        for table in tables:
+            destination_tag: Tag = table.find(
+                "div", {"class": "row text-center"})
+            destination_text = destination_tag.text.strip()
+            time_data = re.findall('([0-9]{2}:[0-9]{2})', str(table))
+            schedule_data[destination_text] = time_data
+
+        logging.info(f'schedule_data {type(schedule_data)} = {schedule_data}')
+        logging.info('Get schedule info completed')
+        return schedule_data
+
+
 if __name__ == "__main__":
-    # https://transport.lanit.ru/m/table
-    # https://transport.lanit.ru/p/table
-    # https://transport.lanit.ru/r/table
-    response = requests.get('https://transport.lanit.ru/m/table')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    to_office = soup.select(
-        "body > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(1)")
-    to_metro = soup.select(
-        "body > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(2)")
-    to_office_time = re.findall('([0-9]{2}:[0-9]{2})', str(to_office))
-    to_metro_time = re.findall('([0-9]{2}:[0-9]{2})', str(to_metro))
+    marina = LanitBusInfo.get_schedule_info(Destinations.MARINA_RISHHA)
+    logging.info(f'marina {type(marina)} = {marina}')
+    ilicha = LanitBusInfo.get_schedule_info(Destinations.PLOSHHAD_ILICHA)
+    logging.info(f'ilicha {type(ilicha)} = {ilicha}')
+    rizhskaja = LanitBusInfo.get_schedule_info(Destinations.RIZHSKAJA)
+    logging.info(f'rizhskaja {type(rizhskaja)} = {rizhskaja}')
