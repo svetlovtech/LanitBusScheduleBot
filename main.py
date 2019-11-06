20b3d0f2049d0190b7e3ffcb642016a2622ea395
32-52 <alexeysvetlov92@gmail.com>
2019-11-06 22:32:23 +0300

    Deleted module LanitBusInfo

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from enum import Enum
 from typing import List
 import os
-import pymorphy2
 import requests
 import telebot
 from bs4 import BeautifulSoup
@@ -29,116 +28,12 @@ if 'token' in bot_token:
 time_delta_shift = 3
 
 
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-
-class Location:
-    def __init__(self, name: str, url: str):
-        self.name = name
-        self.location_char = url
-
-    def __str__(self):
-        return f'Location | name:{self.name} location_char:{self.location_char}'
-
-
-class Locations(Enum):
-    MARINA_RISHHA = Location(
-        'Марьина роща', 'm')
-    PLOSHHAD_ILICHA = Location(
-        'Площадь Ильича', 'p')
-    RIZHSKAJA = Location('Рижская', 'r')
-
-
-class Destinations(Enum):
-    TO_METRO = 'к метро'
-    TO_OFFICE = 'в офис'
-
-
-def mint(word, number):
-    morph = pymorphy2.MorphAnalyzer()
-    text = morph.parse(word)[0]
-    return text.make_agree_with_number(number).word
-
 
 def keyboard_after_all():
     markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     markup.add("/start")
     return markup
 
-
-class LanitBusInfo:
-    @staticmethod
-    def get_formated_datetime_text() -> str:
-        days = ["понедельник", "вторник", "среда",
-                "четверг", "пятница", "суббота", "воскресенье"]
-        current_datetime = datetime.now() + timedelta(hours=time_delta_shift)
-        formated_current_time = f'{str(current_datetime.hour).zfill(2)}:{str(current_datetime.minute).zfill(2)}'
-        return f'Сейчас {days[datetime.today().weekday()]} {formated_current_time}'
-
-    @staticmethod
-    def get_schedule_info(location: Locations) -> dict:
-        logging.info('Get schedule info started...')
-        response = requests.get(
-            f'https://transport.lanit.ru/{location.value.location_char}/table')
-
-        current_datetime = datetime.now() + timedelta(hours=time_delta_shift)
-        soup = BeautifulSoup(response.text, 'lxml')
-        tables: List[Tag] = soup.findAll("div", {"class": "col-xs-6"})
-        schedule_data = {}
-        for table in tables:
-            location_tag: Tag = table.find(
-                "div", {"class": "row text-center"})
-            destination_text = location_tag.text.strip().lower()
-            unparsed_time_data = re.findall('([0-9]{2}:[0-9]{2})', str(table))
-            parsed_time_data = []
-            for bus_time in unparsed_time_data:
-                hour, minute = bus_time.split(':')
-                bus_datetime = datetime(year=current_datetime.year,
-                                        month=current_datetime.month,
-                                        day=current_datetime.day,
-                                        hour=int(hour),
-                                        minute=int(minute),
-                                        second=0)
-                logging.debug(
-                    f'bus_datetime {type(bus_datetime)} = {bus_datetime}')
-                parsed_time_data.append(bus_datetime)
-            schedule_data[Destinations(destination_text)] = parsed_time_data
-
-        logging.info(f'schedule_data {type(schedule_data)} = {schedule_data}')
-        logging.info('Get schedule info completed')
-        return schedule_data
-
-    @staticmethod
-    def get_nearest_bus(location: Locations, destinations: Destinations) -> str:
-        logging.info('Getting nearest bus started...')
-        current_datetime = datetime.now() + timedelta(hours=time_delta_shift)
-        if datetime.today().weekday() > 4:
-            logging.info('Getting nearest bus completed')
-            return f'{LanitBusInfo.get_formated_datetime_text()}. Сегодня маршруток ' \
-                   f'{destinations.value} {location.value.name} не будет.'
-        else:
-            schedule_data = LanitBusInfo.get_schedule_info(location)
-            if len(schedule_data[destinations]) > 0:
-                for bus_datetime in schedule_data[destinations]:
-                    if current_datetime < bus_datetime:
-                        formated_bus_time = f'{str(bus_datetime.hour).zfill(2)}:{str(bus_datetime.minute).zfill(2)}'
-                        time_difference = bus_datetime - current_datetime
-                        time_difference_in_minutes = time_difference.total_seconds() / 60
-                        logging.info('Getting nearest bus completed')
-                        a = mint("минута", int(time_difference_in_minutes))
-                        return f'{LanitBusInfo.get_formated_datetime_text()}. Ближайшая маршрутка' \
-                               f' {destinations.value} {location.value.name} будет через' \
-                               f' {int(time_difference_in_minutes)} {a} в {formated_bus_time}'
-                logging.info('Getting nearest bus completed')
-                return f'{LanitBusInfo.get_formated_datetime_text()}. Сегодня маршруток {destinations.value}' \
-                       f' от {location.value.name} уже не будет.'
-            else:
-                logging.info('Getting nearest bus completed')
-                return f'{LanitBusInfo.get_formated_datetime_text()}. К сожалению не удалось получить расписание' \
-                       f' маршруток {destinations.value} {location.value.name}.'
-
-
-# -=-=-=-=-=-=-=-=-=-=-=Telegram bot part=-=-=-=-=-=-=-=-=-=-=-
 bot = telebot.TeleBot(bot_token)
 
 
