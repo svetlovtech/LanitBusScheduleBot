ce4ad03f533d500de4d166249e97d9eefacde8d0
32-52 <alexeysvetlov92@gmail.com>
2019-11-07 11:12:47 +0300

    Created method from api transport.lanit.ru

--- bus_schedule.py
+++ bus_schedule.py
@@ -1,38 +1,59 @@
-from settings import logging, Locations, Destinations, Location
 from datetime import datetime, timedelta
+from settings import logging
+from enum import Enum
 
 import requests
 import settings
 
 
+class Locations(Enum):
+    MARINA_RISHHA = 'm'
+    PLOSHHAD_ILICHA = 'p'
+    RIZHSKAJA = 'r'
+
+
+class Destination:
+    def __init__(self, api_string: str, translate: str):
+        self.api_string = api_string
+        self.translate = translate
+
+    def __str__(self):
+        return f'Destination | api_string:{self.api_string} translate:{self.translate}'
+
+
+class Destinations(Enum):
+    TO_METRO = Destination('to_metro', 'к')
+    TO_OFFICE = Destination('to_office', 'в')
+
+
 class LanitBusInfo:
     @staticmethod
-    def get_nearest_bus(location: Locations, destinations: Destinations) -> str:
+    def get_nearest_bus(location: Locations, destination: Destinations) -> str:
         logging.info('Getting nearest bus started...')
+        print(location)
         current_datetime = datetime.now() + timedelta(hours=settings.time_delta_shift)
         response = requests.get(
-            f'https://transport.lanit.ru/api/times/{location.value.location_char}').json()
-
+            f'https://transport.lanit.ru/api/times/{location.value}').json()
         if datetime.today().weekday() > 4:
+            message_format = f'Сейчас {settings.days[datetime.today().weekday()]} {response["info"]["now"]}. ' \
+                             f'Сегодня маршруток {destination.value.translate} {response["info"]["name"]} не будет.'
+            logging.debug(f'message_format {type(message_format)} = {message_format}')
             logging.info('Getting nearest bus completed')
-            return f'Сейчас {settings.days[datetime.today().weekday()]} {response["info"]["now"]}. Сегодня маршруток ' \
-                   f'{destinations.value} {response["info"]["name"]} не будет.'
+            return message_format
+        elif response['time'][destination.value.api_string]['nearest'] is not False:
+            message_format = f'Сейчас {settings.days[datetime.today().weekday()]} {response["info"]["now"]}. ' \
+                             f'Ближайшая маршрутка {destination.value.translate} ' \
+                             f'{response["info"]["name"]} будет через {response["time"][destination.value.api_string]["left"]}' \
+                             f' в {response["time"][destination.value.api_string]["nearest"]}.'
+            logging.debug(f'message_format {type(message_format)} = {message_format}')
+            logging.info('Getting nearest bus completed')
+            return message_format
+        elif response['time'][destination.value.api_string]['nearest'] is False:
+            message_format = f'Сейчас {settings.days[datetime.today().weekday()]} {response["info"]["now"]}. ' \
+                             f'Сегодня маршруток {destination.value.translate} {response["info"]["name"]} не будет.'
+            logging.debug(f'message_format {type(message_format)} = {message_format}')
+            logging.info('Getting nearest bus completed')
+            return message_format
         else:
-            schedule_data = LanitBusInfo.get_schedule_info(location)
-            if len(schedule_data[destinations]) > 0:
-                for bus_datetime in schedule_data[destinations]:
-                    if current_datetime < bus_datetime:
-                        formated_bus_time = f'{str(bus_datetime.hour).zfill(2)}:{str(bus_datetime.minute).zfill(2)}'
-                        time_difference = bus_datetime - current_datetime
-                        time_difference_in_minutes = time_difference.total_seconds() / 60
-                        logging.info('Getting nearest bus completed')
-                        return f'{LanitBusInfo.get_formated_datetime_text()}. Ближайшая маршрутка' \
-                               f' {destinations.value} {location.value.name} будет через' \
-                               f' {int(time_difference_in_minutes)} {response["info"]["name"]} в {formated_bus_time}'
-                logging.info('Getting nearest bus completed')
-                return f'{LanitBusInfo.get_formated_datetime_text()}. Сегодня маршруток {destinations.value}' \
-                       f' от {location.value.name} уже не будет.'
-            else:
-                logging.info('Getting nearest bus completed')
-                return f'{LanitBusInfo.get_formated_datetime_text()}. К сожалению не удалось получить расписание' \
-                       f' маршруток {destinations.value} {location.value.name}.'
+            message_format = 'К сожалению не удалось получить расписание'
+            return message_format
